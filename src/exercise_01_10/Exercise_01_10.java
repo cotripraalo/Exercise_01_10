5bfe5b44d7d4f6516a4afba63d6141f7635a6bf3
ALONDRA COTRINA <ALONDRA COTRINA@ALONDRACOTRINA>
2017-09-06 22:27:14 -0500

    firts

---
+++ src/exercise_01_10/Exercise_01_10.java
@@ -0,0 +1,11 @@
+/*
+(Average speed in miles) Assume a runner runs 14 kilometers in 45 minutes and 30
+seconds. Write a program that displays the average speed in miles per hour. (Note
+that 1 mile is 1.6 kilometers.)
+*/
+package exercise_01_10;
+public class Exercise_01_10 {
+    public static void main(String[] args) {
+        System.out.println((14 / 45.30)/ 1.6);
+    }
+}
